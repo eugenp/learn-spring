0681e7f79eda03f32606895827241854ddbd445f
Loredana <lore.crusoveanu@gmail.com>
2019-05-01 13:28:11 +0300

    add module 6

---
+++ m6-what-is-spring-mvc-lesson-end/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
@@ -0,0 +1,20 @@
+package com.baeldung.ls.web.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.ls.persistence.model.Project;
+
+@RestController
+@RequestMapping(value = "/projects")
+public class ProjectController {
+
+	@GetMapping(path = "/1")
+	public Project findOne() {
+		return new Project(0l, "testName", LocalDate.now());
+	}
+
+}
