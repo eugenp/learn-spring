705c0010bde899323d8fa03fe4fbeb2dffaae24a
Loredana <lore.crusoveanu@gmail.com>
2019-06-08 21:13:31 +0300

    add parent pom

--- m1-real-world-project/src/main/java/com/baeldung/ls/LsApp.java
+++ m1-real-world-project/src/main/java/com/baeldung/ls/LsApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LsApp {
 
-	public static void main(final String... args) {
-		SpringApplication.run(LsApp.class, args);
-	}
+    public static void main(final String... args) {
+        SpringApplication.run(LsApp.class, args);
+    }
 
 }
