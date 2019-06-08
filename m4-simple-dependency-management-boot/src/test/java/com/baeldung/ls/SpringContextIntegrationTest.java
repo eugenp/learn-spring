a8aafaa531878dc012dd1ed0f679b97e210c4697
Loredana <lore.crusoveanu@gmail.com>
2019-06-08 21:17:27 +0300

    update constructors, test

---
+++ m4-simple-dependency-management-boot/src/test/java/com/baeldung/ls/SpringContextIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.baeldung.ls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringContextIntegrationTest {
+
+    @Test
+    public void whenContextIsLoaded_thenNoExceptions() {
+    }
+}
