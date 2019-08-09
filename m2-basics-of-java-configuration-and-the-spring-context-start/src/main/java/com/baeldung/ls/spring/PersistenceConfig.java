81bbeff7ccc14c29d1ccc1a4188fb462fe22ff36
carloszr78 <>
2019-08-09 10:24:30 +0200

    no message

---
+++ m2-basics-of-java-configuration-and-the-spring-context-start/src/main/java/com/baeldung/ls/spring/PersistenceConfig.java
@@ -0,0 +1,16 @@
+package com.baeldung.ls.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baeldung.ls.persistence.repository.impl.ProjectRepositoryImpl;
+
+@Configuration
+public class PersistenceConfig {
+    
+    @Bean
+    public ProjectRepositoryImpl projectRepository() {
+        return new ProjectRepositoryImpl();
+    }
+
+}
