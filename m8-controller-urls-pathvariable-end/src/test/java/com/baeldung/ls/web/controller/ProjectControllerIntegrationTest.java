eba9c5552d82b86bb9f62f609ee1f1b5ee6c9f8e
Loredana <lore.crusoveanu@gmail.com>
2020-03-01 22:23:34 +0200

    boot upgrade

--- m8-controller-urls-pathvariable-end/src/test/java/com/baeldung/ls/web/controller/ProjectControllerIntegrationTest.java
+++ m8-controller-urls-pathvariable-end/src/test/java/com/baeldung/ls/web/controller/ProjectControllerIntegrationTest.java
@@ -10,15 +10,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProjectControllerIntegrationTest {
--- m8-exception-handling-start/src/test/java/com/baeldung/ls/web/controller/ProjectControllerIntegrationTest.java
+++ m8-exception-handling-start/src/test/java/com/baeldung/ls/web/controller/ProjectControllerIntegrationTest.java
@@ -15,12 +15,10 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -31,7 +29,6 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProjectControllerIntegrationTest {
