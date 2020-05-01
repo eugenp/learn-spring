0054c4f19edb987c28136d40ccb6bbf2b5db27b6
Loredana <lore.crusoveanu@gmail.com>
2020-05-01 13:25:22 +0300

    remove ref project from m8

--- m8-reference-project/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.ls.persistence.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.baeldung.ls.persistence.model.Project;
-
-public interface IProjectRepository extends PagingAndSortingRepository<Project, Long> {
-    Iterable<Project> findByNameContaining(String name);
-}
