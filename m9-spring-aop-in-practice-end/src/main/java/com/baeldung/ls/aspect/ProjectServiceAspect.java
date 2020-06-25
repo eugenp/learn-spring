c2906ac5c2bc07642237e12ecd6c1ba8ffd45802
Loredana <lore.crusoveanu@gmail.com>
2020-06-25 14:20:46 +0300

    add proxies lesson, add aop examples

--- m9-spring-aop-in-practice-end/src/main/java/com/baeldung/ls/aspect/ProjectServiceAspect.java
+++ m9-spring-aop-in-practice-end/src/main/java/com/baeldung/ls/aspect/ProjectServiceAspect.java
@@ -1,11 +1,15 @@
 package com.baeldung.ls.aspect;
 
+import java.util.Optional;
+
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.baeldung.ls.persistence.model.Project;
 
 @Aspect
 @Component
@@ -18,4 +22,27 @@ public class ProjectServiceAspect {
         LOG.info("Searching Project with Id {}", joinPoint.getArgs()[0]);
     }
 
+    @After("within(com.baeldung.ls.service.impl.ProjectServiceImpl)")
+    public void afterAllMethodsOfProjectServiceImpl(JoinPoint joinPoint) {
+        LOG.info("After invoking the method: {} ", joinPoint.getSignature()
+            .getName());
+    }
+
+    @AfterReturning(pointcut = "execution(*..Optional<*..Project> *..service..findById(*))", returning = "project")
+    public void afterReturningProject(Optional<Project> project) {
+        LOG.info("project found: {}", project.orElse(null));
+    }
+
+    @Around("execution(* com.baeldung.ls.service.impl.ProjectServiceImpl.save(*))")
+    public Object aroundSave(ProceedingJoinPoint joinPoint) {
+        Object val = joinPoint.getArgs()[0];
+        try {
+            LOG.info("saving project : {}", val);
+            val = joinPoint.proceed();
+            LOG.info("project saved successfully !!");
+        } catch (Throwable e) {
+            LOG.error("error while saving project: ", e);
+        }
+        return val;
+    }
 }
---
+++ spring-proxies-end/src/main/java/com/baeldung/ls/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.baeldung.ls.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig {
+
+}
