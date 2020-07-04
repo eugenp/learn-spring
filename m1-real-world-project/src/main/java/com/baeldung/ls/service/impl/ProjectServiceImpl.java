652fefdb298ee722411a5a0b568f6190c9bafb82
Loredana <lore.crusoveanu@gmail.com>
2020-07-04 14:25:34 +0300

    boot upgrade

--- m1-real-world-project/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
+++ m1-real-world-project/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
@@ -3,23 +3,41 @@ package com.baeldung.ls.service.impl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.baeldung.ls.persistence.model.Project;
 import com.baeldung.ls.persistence.repository.IProjectRepository;
 import com.baeldung.ls.service.IProjectService;
 
+@Service
 public class ProjectServiceImpl implements IProjectService {
 
     @Autowired
-    private IProjectRepository projectRepo;
+    private IProjectRepository projectRepository;
+
+    public ProjectServiceImpl() {
+    }
+
+    public ProjectServiceImpl(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 
     @Override
     public Optional<Project> findById(Long id) {
-        return projectRepo.findById(id);
+        return projectRepository.findById(id);
     }
 
     @Override
     public Project save(Project project) {
-        return projectRepo.save(project);
+        return projectRepository.save(project);
     }
+
+    public IProjectRepository getProjectRepository() {
+        return projectRepository;
+    }
+
+    public void setProjectRepository(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
 }
