48766e310873e4cd1e606b32818f90f4bbb05227
Loredana <lore.crusoveanu@gmail.com>
2020-07-04 14:28:45 +0300

    boot upgrade

--- m7-view-resolvers-templating-engines-end/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
+++ m7-view-resolvers-templating-engines-end/src/main/java/com/baeldung/ls/web/controller/ProjectController.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.baeldung.ls.persistence.model.Project;
 import com.baeldung.ls.persistence.model.Task;
@@ -33,6 +38,19 @@ public class ProjectController {
         projects.forEach(p -> projectDtos.add(convertToDto(p)));
         model.addAttribute("projects", projectDtos);
         return "projects";
+    }
+
+    @GetMapping(value = "/{id}")
+    public ProjectDto findOne(@PathVariable Long id) {
+        Project entity = projectService.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToDto(entity);
+    }
+
+    @PostMapping
+    public void create(@RequestBody ProjectDto newProject) {
+        Project entity = convertToEntity(newProject);
+        this.projectService.save(entity);
     }
 
     protected ProjectDto convertToDto(Project entity) {
