94ec00f7e152b4fe367c273e43b133cbcc3a347b
Loredana <lore.crusoveanu@gmail.com>
2020-08-25 14:08:20 +0300

    new lesson auth

---
+++ spring-security-authorization-end/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,56 @@
+package com.baeldung.ls.service.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.baeldung.ls.persistence.model.Project;
+import com.baeldung.ls.persistence.model.Task;
+import com.baeldung.ls.persistence.repository.IProjectRepository;
+import com.baeldung.ls.service.IProjectService;
+
+@Service
+public class ProjectServiceImpl implements IProjectService {
+
+    private IProjectRepository projectRepository;
+
+    public ProjectServiceImpl(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    @Override
+    public Project save(Project project) {
+        if (StringUtils.isEmpty(project.getId())) {
+            project.setDateCreated(LocalDate.now());
+        }
+        return projectRepository.save(project);
+    }
+
+    @Override
+    @PreAuthorize("hasRole('MANAGER')")
+    public Iterable<Project> findAll() {
+        return projectRepository.findAll();
+    }
+
+    @Override
+    public Project addTasks(Project project, List<Task> tasks) {
+        project.getTasks()
+            .addAll(tasks.stream()
+                .filter(t -> !StringUtils.isEmpty(t.getName()))
+                .collect(Collectors.toList()));
+        projectRepository.save(project);
+
+        return project;
+    }
+
+}
