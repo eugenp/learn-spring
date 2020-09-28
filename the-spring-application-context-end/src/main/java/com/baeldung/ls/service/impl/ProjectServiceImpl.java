fefcc656973bff428abb8e8191107f3fa712ef51
Loredana <lore.crusoveanu@gmail.com>
2020-09-28 17:18:18 +0300

    upgrade boot version

--- the-spring-application-context-end/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
+++ the-spring-application-context-end/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
