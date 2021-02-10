c84e90eb55e8f9163f168ada4cf8f942d9920f08
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2021-02-10 14:08:30 +0200

    fix mysql setup

--- m5-spring-with-jpa-and-hibernate-end/src/main/java/com/baeldung/ls/config/PersistenceConfig.java
+++ m5-spring-with-jpa-and-hibernate-end/src/main/java/com/baeldung/ls/config/PersistenceConfig.java
@@ -1,10 +1,15 @@
 package com.baeldung.ls.config;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class PersistenceConfig {
@@ -19,8 +24,8 @@ public class PersistenceConfig {
         return dataSource;
     }
 
-    /* MySQL DataSource bean
-    @Bean
+    //  MySQL DataSource bean
+    /*@Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
@@ -28,5 +33,22 @@ public class PersistenceConfig {
         dataSource.setUsername("username");
         dataSource.setPassword("password");
         return dataSource;
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+      em.setDataSource(dataSource);
+      em.setPackagesToScan("com.baeldung.ls.persistence.model");
+
+
+      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      em.setJpaVendorAdapter(vendorAdapter);
+
+      Properties properties = new Properties();
+      properties.setProperty("hibernate.hbm2ddl.auto", "update");
+      em.setJpaProperties(properties);
+
+      return em;
     }*/
 }
