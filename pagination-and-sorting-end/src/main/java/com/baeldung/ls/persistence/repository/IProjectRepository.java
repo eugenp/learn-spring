b4113e217e2aabdec00a8d1e6199bc959645a293
Ger Roza <rozagerardo@gmail.com>
2023-02-22 14:24:15 -0300

    Upgraded to Boot 3

--- pagination-and-sorting-end/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
+++ pagination-and-sorting-end/src/main/java/com/baeldung/ls/persistence/repository/IProjectRepository.java
@@ -4,15 +4,17 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.baeldung.ls.persistence.model.Project;
 
-public interface IProjectRepository extends PagingAndSortingRepository<Project, Long> {
+public interface IProjectRepository extends PagingAndSortingRepository<Project, Long>, CrudRepository<Project, Long> {
 
     Optional<Project> findByName(String name);
 
     List<Project> findByDateCreatedBetween(LocalDate start, LocalDate end);
 
     List<Project> findAllByOrderByNameDesc();
+
 }
--- spring-with-jpa-and-hibernate-end/src/main/java/com/baeldung/ls/persistence/repository/impl/ProjectRepositoryImpl.java
+++ spring-with-jpa-and-hibernate-end/src/main/java/com/baeldung/ls/persistence/repository/impl/ProjectRepositoryImpl.java
@@ -2,8 +2,8 @@ package com.baeldung.ls.persistence.repository.impl;
 
 import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
