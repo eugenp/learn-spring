cc370fddc274dad60f112498514895940639c97e
Ger Roza <rozagerardo@gmail.com>
2023-04-14 12:12:14 -0300

    Adapted code for new Problem Details spec support

--- exception-handling-end/src/main/java/com/baeldung/ls/web/errors/ProblemDetailsGlobalExceptionHandler.java
+++ exception-handling-end/src/main/java/com/baeldung/ls/web/errors/ProblemDetailsGlobalExceptionHandler.java
@@ -1,23 +1,25 @@
 package com.baeldung.ls.web.errors;
 
 import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+//@ControllerAdvice
+public class ProblemDetailsGlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DataRetrievalFailureException.class)
     public ResponseEntity<String> handleDataRetrievalException(DataRetrievalFailureException ex) {
         return new ResponseEntity<String>("Exception retrieving data: " + ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    // if we avoid using the ResponseEntityExceptionHandler we have to map the exceptions ourselves
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<String> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException ex) {
+    @Override
+    public ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         return new ResponseEntity<>("Media Type not supported: " + ex.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 }
---
+++ exception-handling-end/src/test/java/com/baeldung/ls/web/controller/ProblemDetailsProjectControllerEndToEndIntegrationTest.java
@@ -0,0 +1,73 @@
+package com.baeldung.ls.web.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.baeldung.alternativecontextconfig.web.errors.ProblemDetailsGlobalExceptionHandler;
+import com.baeldung.ls.LsApp;
+import com.baeldung.ls.persistence.model.Project;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = { LsApp.class, ProblemDetailsGlobalExceptionHandler.class })
+public class ProblemDetailsProjectControllerEndToEndIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void givenDefaultProjectsPersisted_whenRequestAllProjects_thenRetrieveListWithEntities() throws Exception {
+        ResponseEntity<Project[]> projectsResponse = this.restTemplate.getForEntity("/projects", Project[].class);
+
+        assertThat(projectsResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(projectsResponse.getBody()).hasSizeGreaterThanOrEqualTo(2);
+    }
+
+    @Test
+    public void whenCreateUsingXmlContentType_thenHandledResponseRetrieved() throws Exception {
+        RequestEntity<Void> request = RequestEntity.post("/projects")
+            .contentType(MediaType.APPLICATION_XML)
+            .build();
+
+        ResponseEntity<String> errorResponse = this.restTemplate.exchange(request, String.class);
+
+        assertThat(errorResponse.getStatusCode()).isEqualTo(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        assertThat(errorResponse.getBody()).startsWith("Media Type not supported: ");
+    }
+
+    @Test
+    public void whenGetNonExistingProject_thenGetEntityRetrieves4xxWithBootErrorFields() throws Exception {
+        RequestEntity<Void> request = RequestEntity.get("/projects/99")
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+
+        ResponseEntity<ProblemDetail> errorResponse = this.restTemplate.exchange(request, ProblemDetail.class);
+
+        assertThat(errorResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(errorResponse.getBody()
+            .getDetail()).isEqualTo("Project not found");
+    }
+
+    @Test
+    public void whenDeleteNonExistingProject_thenGetEntityRetrieves4xx() throws Exception {
+        RequestEntity<Void> request = RequestEntity.delete("/projects/99")
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+
+        ResponseEntity<String> errorResponse = this.restTemplate.exchange(request, String.class);
+
+        assertThat(errorResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(errorResponse.getBody()).isEqualTo("Exception retrieving data: No class com.baeldung.ls.persistence.model.Project entity with id 99 exists");
+    }
+
+}
