068dc7b9f7ad609598b7c04204489f8316febb5f
Ger Roza <rozagerardo@gmail.com>
2023-05-17 19:23:02 -0300

    Removed annotation without effect

--- spring-proxies-end/src/main/java/com/baeldung/ls/web/dto/TaskDto.java
+++ spring-proxies-end/src/main/java/com/baeldung/ls/web/dto/TaskDto.java
@@ -2,15 +2,13 @@ package com.baeldung.ls.web.dto;
 
 import java.time.LocalDate;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.baeldung.ls.persistence.model.TaskStatus;
 
 public record TaskDto(
     Long id,
     String name,
     String description,
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateCreated,
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dueDate,
+    LocalDate dateCreated,
+    LocalDate dueDate,
     TaskStatus status) {
 }
