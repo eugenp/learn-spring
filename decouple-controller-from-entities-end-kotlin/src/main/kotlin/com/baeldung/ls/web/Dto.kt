50f721719ae92797af43b421cdae904f1de4dc40
Ger Roza <rozagerardo@gmail.com>
2023-05-17 19:15:35 -0300

    Removed annotation without effect

--- decouple-controller-from-entities-end-kotlin/src/main/kotlin/com/baeldung/ls/web/Dto.kt
+++ decouple-controller-from-entities-end-kotlin/src/main/kotlin/com/baeldung/ls/web/Dto.kt
@@ -1,7 +1,6 @@
 package com.baeldung.ls.web
 
 import com.baeldung.ls.persistence.TaskStatus
-import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
 
 class ProjectDto(
@@ -15,9 +14,7 @@ class TaskDto(
     var id: Long? = null,
     var name: String,
     var description: String? = null,
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     var dateCreated: LocalDate? = null,
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     var dueDate: LocalDate? = null,
     var status: TaskStatus?,
 )
