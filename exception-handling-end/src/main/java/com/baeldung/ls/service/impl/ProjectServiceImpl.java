ccfe4a7d17cc0825648a030e6acb216c5a702527
Ger Roza <rozagerardo@gmail.com>
2023-06-17 19:25:48 -0300

    Spring Boot 3 support changes - JPA repository delete behavior update

--- exception-handling-end/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
+++ exception-handling-end/src/main/java/com/baeldung/ls/service/impl/ProjectServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.baeldung.ls.persistence.model.Project;
@@ -39,6 +40,8 @@ public class ProjectServiceImpl implements IProjectService {
 
     @Override
     public void delete(Long id) {
+        this.findById(id)
+                .orElseThrow(() -> new EmptyResultDataAccessException(String.format("No class com.baeldung.ls.persistence.model.Project entity with id %s exists", id), 1));
         projectRepository.deleteById(id);
     }
 
